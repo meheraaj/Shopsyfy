6819890d9422ed07f47d53816cba78f776f976e7
Meheraj <71807497+meheraaj@users.noreply.github.com>
2025-11-09 20:37:36 +0600

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,41 +24,42 @@ function fillTrendingProd(prod) {
              <div class="sale__item">
                                 <div class="product__wrapper mb-16">
                                     <div class="product__thumb">
-                                        <a href="product-details.html" class="w-img block">
+                                        <div  class="w-img block">
                                             <img src="${prod[i]["image1"]}"
                                                 alt="product-img" draggable="false">
                                             <img class="product__thumb-2"
                                                 src="${prod[i]["image2_hover"]}"
                                                 alt="product-img" draggable="false">
-                                        </a>
+                                        </div>
                                         <div class="product__action transition-3">
-                                             <button href="#" data-bs-toggle="tooltip" data-placement="top" class='addtowishlist' data-product='${JSON.stringify(
+                                             <button  data-bs-toggle="tooltip" data-placement="top" class='addtowishlist' data-product='${JSON.stringify(
                                                p
                                              ).replace(/'/g, "&apos;")}'
                                                 title="Add to Wishlist">
                                                 <i class="fal fa-heart"></i>
                                             </button>
-                                            <a href="#" data-bs-toggle="tooltip" data-placement="top" title="Compare">
+                                            <button  data-bs-toggle="tooltip" data-placement="top" title="Compare">
                                                 <i class="fal fa-sliders-h"></i>
-                                            </a>
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#productModalId">
+                                            </button>
+                                            <button data-bs-toggle="modal" data-bs-target="#productModalId">
                                                 <i class="fal fa-search"></i>
-                                            </a>
+                                            </button>
                                         </div>
                                          <div class="product__sale">
                                             <span class="new">${
-                                              prod[i]["saleInfo"]["new"]
+                                              prod[i]["saleInfo"]["new"] ?? ""
                                             }</span>
                                             <span class="percent">${
-                                              prod[i]["saleInfo"]["percent"]
+                                              prod[i]["saleInfo"]["percent"] ??
+                                              ""
                                             }</span>
                                         </div>
                                     </div>
                                     <div class="product__content relative">
                                         <div class="product__content-inner">
-                                            <h4><a href="product-details.html">${
+                                            <h4><button>${
                                               prod[i]["title"]
-                                            }</a></h4>
+                                            }</button></h4>
                                             <div class="product__price transition-3">
                                                 <span>${prod[i]["price"]}</span>
                                                 <span class="old-price">${
@@ -96,7 +97,7 @@ async function fillDatas() {
  <div class="sale__item flex-shrink-0 ">
                                 <div class="product__wrapper mb-16">
                                     <div class="product__thumb">
-                                        <a href="product-details.html" class="w-img block">
+                                        <div class="w-img block">
                                             <img src="${
                                               myContent["saleOffProducts"][i][
                                                 "image1"
@@ -110,18 +111,18 @@ async function fillDatas() {
                                                   ]["image2_hover"]
                                                 }"
                                                 alt="product-img" draggable="false">
-                                        </a>
+                                        </div>
                                         <div class="product__action transition-3">
-                                            <button href="#" data-bs-toggle="tooltip" data-placement="top" class='addtowishlist' data-product='${JSON.stringify(
+                                            <button  data-bs-toggle="tooltip" data-placement="top" class='addtowishlist' data-product='${JSON.stringify(
                                               p
                                             ).replace(/'/g, "&apos;")}'
                                                 title="Add to Wishlist">
                                                 <i class="fal fa-heart"></i>
                                             </button>
-                                            <a href="#" data-bs-toggle="tooltip" data-placement="top" title="Compare">
+                                            <a href="" data-bs-toggle="tooltip" data-placement="top" title="Compare">
                                                 <i class="fal fa-sliders-h"></i>
                                             </a>
-                                            <a href="#" data-bs-toggle="modal" data-bs-target="#productModalId">
+                                            <a href="" data-bs-toggle="modal" data-bs-target="#productModalId">
                                                 <i class="fal fa-search"></i>
                                             </a>
                                         </div>
@@ -140,11 +141,11 @@ async function fillDatas() {
                                     </div>
                                     <div class="product__content relative">
                                         <div class="product__content-inner">
-                                            <h4><a href="shop-details.html">${
+                                            <h4><button">${
                                               myContent["saleOffProducts"][i][
                                                 "title"
                                               ]
-                                            }</a></h4>
+                                            }</button></h4>
                                             <div class="product__price transition-3">
                                                 <span>${
                                                   myContent["saleOffProducts"][
@@ -178,7 +179,7 @@ async function fillDatas() {
 
             <div class="swiper-slide blog__item h-auto">
                               <div class="blog__thumb overflow-hidden w-full h-auto">
-                                  <a href="blog-details.html" class="w-img ">
+                                  <a href="" class="w-img ">
                                       <img src="${myContent["blogPosts"][i]["image"]}" alt="${myContent["blogPosts"][i]["title"]}" draggable="false"
                                           class="w-full h-auto object-cover transition-transform duration-300 hover:scale-[1.05]">
                                   </a>
@@ -191,7 +192,7 @@ async function fillDatas() {
                                       <span class="ml-3">${myContent["blogPosts"][i]["date"]}</span>
                                   </div>
                                   <p class="text-sm text-gray-600 mb-5">${myContent["blogPosts"][i]["excerpt"]}</p>
-                                  <a href="blog-details.html"
+                                  <a href=""
                                       class="os-btn !w-auto bg-transparent border-gray-300 text-gray-900 hover:text-white hover:bg-bc8246 hover:border-bc8246">read
                                       more</a>
                               </div>
